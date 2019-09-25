57d2cc442a5ab6efa3084b67094ea4083e56f892
gkumarswamy <gkumarswamy16@gmail.com>
2019-09-25 16:13:25 +0530

    Sec Tech Scroll

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef , HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  techScrollView : string = 'hide';
+  constructor(public el: ElementRef) { }
 
   ngOnInit() {
+  }
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    console.log(componentPosition);
+    const scrollPosition = window.pageYOffset
+    console.log(scrollPosition);
+    if (scrollPosition > 2995) {
+      this.techScrollView = 'boxTechView'
+    } else {
+      this.techScrollView = 'boxTechView-close'
+    }
   }
 
 }
