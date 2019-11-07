d2eb6105adf6b4f80fa8b140df66395fadc70fd7
gkumarswamy <gkumarswamy16@gmail.com>
2019-11-07 16:03:37 +0530

    header scroll completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef , HostListener } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,44 @@ export class AppComponent {
   title = 'sssbi-dev';
   showservices="close";
   menu = "close";
+  headerScrollView = 'scrollHeader-close';
+  pagename = '';
+
+  
+  constructor(public el: ElementRef, private router: Router, private activatedRoute: ActivatedRoute) { }
+  ngOnInit() {
+    // this.router.events.filter((event: RouterEvent) => event instanceof NavigationStart)
+    // .subscribe((x) => {
+    //   if (x['url'].indexOf('admin/') > -1) {
+    //       this.pagename = false;
+    //   } else {
+    //     this.pagename = true;
+    //   }
+    // });
+    
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((x) => {
+      console.log(x['url'].indexOf('/contact'));
+      if (x['url'].indexOf('/contact') > -1) {
+              this.pagename = 'contactus';
+          } else {
+            this.pagename = '';
+          }
+    });
+  }
+  
+  @HostListener('window:scroll', ['$event'])
+  checkScroll(e) {
+    const scrollPosition = window.pageYOffset
+    console.log(scrollPosition);
+    if (scrollPosition > 200) {
+      this.headerScrollView = 'scrollHeader-open'
+    } else {
+      this.headerScrollView = 'scrollHeader-close'
+    }
+  }
+
   OpenMenuModel(){
     if(this.menu == 'close'){
       this.menu = 'open';
